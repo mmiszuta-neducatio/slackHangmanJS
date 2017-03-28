4b2da5673e2dd0ae899cd6777c41c3e4cfad9556
mzielinski-neducatio <mzielinski@n-educatio.pl>
2017-03-28 14:44:00 +0200

    added bot responses functionality @mmiszuta-neducatio @mzielinski-neducatio

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const bot = new Bot({
   autoMark: true
 });
 
-request = request.defaults({jar: true});
+request = request.defaults({
+  jar: true
+});
 
 bot.respondTo('help', (message, channel) => {
   bot.send('To start a new game type \'hangman\', to guess a letter (for example x) type \'letter: x\'', channel);
@@ -17,15 +19,17 @@ bot.respondTo('help', (message, channel) => {
 
 bot.respondTo('hangman', (message, channel) => {
   request({
-    url: 'http://35.156.165.137:3000/game',
+    url: ' http://35.158.79.64:3000/game',
     method: 'GET',
-}, (err, res, body) => {
+  }, (err, res, body) => {
     if (err) {
       console.error(err);
-      return;
+      bot.send('An error occurred: ' + err, channel);
     }
     let JSONresponse = JSON.parse(body);
-    bot.send(Array(JSONresponse.guessedLetters.length + 1).join('_') + '\n' + 'remainingAttempts:', channel);
+    bot.send(Array(JSONresponse.guessedLetters.length + 1).join('_') + '\n' + 'remaining attempts:' +
+      JSONresponse.remainingAttempts + '\n' + 'games won ' + JSONresponse.gameStats.won + '\n' + 'games lost ' +
+      JSONresponse.gameStats.lost, channel);
   });
 }, true);
 
@@ -40,23 +44,31 @@ bot.respondTo('letter:', (message, channel) => {
     return;
   }
   request({
-    url: `http://35.156.165.137:3000/letter/${letterToGuess}`,
+    url: `http://35.158.79.64:3000/letter/${letterToGuess}`,
     method: 'GET',
-}, (err, res, body) => {
+  }, (err, res, body) => {
     if (err) {
       console.error(err);
-      return;
+      bot.send('An error occurred: ' + err, channel);
     }
     let JSONresponse = JSON.parse(body);
-    console.log(JSONresponse);
     let result = new Array(JSONresponse.guessedLetters.length);
-    for(let i = 0; i < JSONresponse.guessedLetters.length; i++) {
-      if(JSONresponse.guessedLetters[i] === null){
+    for (let i = 0; i < JSONresponse.guessedLetters.length; i++) {
+      if (JSONresponse.guessedLetters[i] === null) {
         result[i] = '_';
         continue;
       }
       result[i] = JSONresponse.guessedLetters[i];
     }
-    bot.send(result.join(''), channel);
+    if (JSONresponse.isValid === false) {
+      bot.send('Not this time, try again', channel);
+    }
+    if (JSONresponse.isGameOver === true) {
+      if (JSONresponse.gameOverType === 'loss') {
+        return bot.send('You have lost try again by typing "hangman"', channel);
+      }
+      return bot.send('You have won - Congrats, you can play again, simply type "hangman"', channel);
+    }
+    bot.send(result.join('') + '\n' + 'You have got ' + JSONresponse.remainingAttempts + ' attempts left', channel);
   });
 }, true);
